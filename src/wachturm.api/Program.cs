b3855d8a76840cda2f4d56482dc6714c708634d3
Elvin Suleymanov <289714146+nachtbit@users.noreply.github.com>
2026-06-10 15:07:16 +0400

    feat(api): integrate Scalar API documentation

--- src/wachturm.api/Program.cs
+++ src/wachturm.api/Program.cs
@@ -1,17 +1,28 @@
+using Scalar.AspNetCore;
+using wachturm.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapSwagger("/openapi/{documentName}.json");
+
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "Wachturm API";
+        options.OpenApiRoutePattern = "/openapi/{documentName}.json";
+    });
 }
 
-app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
